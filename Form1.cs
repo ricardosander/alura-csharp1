3d44c6d6930f878c3e08785c0b212e9ed5729c80
Rick Sander <Rick Sander>
2017-02-19 12:46:32 -0300

    Criando botão que exibe mensagem ao ser clicado.

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace OiMundo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Meu primeiro programa C#");
+        }
     }
 }
